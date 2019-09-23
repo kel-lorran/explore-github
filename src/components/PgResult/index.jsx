bda3ec2190cc668cdfe0180792bcea0637bc0923
kel-lorran <kelvinlorran_arq@hotmail.com>
2019-09-23 00:56:03 -0300

    ajustes mobile, o nome dos repositorios é muito extenso para exibicao sem quebra de linha

--- src/components/PgResult/index.jsx
+++ src/components/PgResult/index.jsx
@@ -96,6 +96,7 @@ class PgResult extends React.Component{
                             title={dataOfRepository.name}
                             description={dataOfRepository.description}
                             numStar={dataOfRepository.star}
+                            forceOverflow={true}
                         />)
                     }
                 </MyList>
