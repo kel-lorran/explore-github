75bf60c0203b525218d5d8204879fd4af005b8a2
kel-lorran <kelvinlorran_arq@hotmail.com>
2019-09-23 01:22:29 -0300

    build v2

---
+++ precache-manifest.87a6628f783809f6d1d44b0f2ab49b0b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4f9b0d4a4299d4dc86997e0010ae5c8",
+    "url": "./index.html"
+  },
+  {
+    "revision": "96082a4b790a9734d103",
+    "url": "./static/css/main.eec6d526.chunk.css"
+  },
+  {
+    "revision": "e65edc1141efe6c3c8e8",
+    "url": "./static/js/2.1fa4193b.chunk.js"
+  },
+  {
+    "revision": "96082a4b790a9734d103",
+    "url": "./static/js/main.51484c7b.chunk.js"
+  },
+  {
+    "revision": "5fc6ae6773914f465595",
+    "url": "./static/js/runtime~main.d51f37df.js"
+  }
+]);
